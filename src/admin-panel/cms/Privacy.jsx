913e4809840fab4f61a425a41ded4b6f6ee8a9c7
Moulik98 <soumamoulik@gmail.com>
2023-09-13 18:28:24 +0530

    init

--- src/admin-panel/cms/Privacy.jsx
+++ src/admin-panel/cms/Privacy.jsx
@@ -9,7 +9,7 @@ const Privacy = () => {
             <div className='p-5'>
                 <section>
                     <div className='max-w-6xl mx-auto flex justify-between py-5'>
-                        <p className='text-2xl text-gray-900 font-semibold'>Privacy</p>
+                        <p className='text-2xl text-gray-900 font-semibold'>Privacy Policy</p>
                         <div className='flex gap-x-10'>
                             <form className="flex items-center">
                                 <div className="flex items-center px-2 py-1 gap-x-1 bg-gray-100 rounded-2xl ">
@@ -43,13 +43,13 @@ const Privacy = () => {
                     </div>
                 </section>
                 {/* General Questions */}
-                <section className='p-5 flex flex-row gap-x-1 border border-[#E8E8E8] rounded'>
-                    <div className='p-1 shrink-0 w-[24rem] flex flex-col space-y-4 '>
+                <section className='p-5 flex flex-row gap-x-1 border bg-blue-950 border-[#E8E8E8] rounded'>
+                    <div className='p-1 shrink-0 w-full  flex flex-col space-y-4 '>
                         <div className='flex flex-row justify-between'>
-                            <p className='text-base text-gray-900 font-semibold'>General Questions</p>
+                            <p className='text-base text-white font-semibold'>General Questions</p>
                             <p
                                 onClick={() => setIsClicked(true)}
-                                className='text-base text-gray-900 font-semibold flex gap-x-2 justify-center items-center cursor-pointer'>Edit
+                                className='text-base text-gray-100 font-semibold flex gap-x-2 justify-center items-center cursor-pointer'>Edit
                                 <span>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4 font-semibold">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
@@ -59,7 +59,7 @@ const Privacy = () => {
                             </p>
                         </div>
                         <div>
-                            <p className='text-sm text-[#878A99] font-normal text-left'>General knowledge is information that has been accumulated over time through various mediums and sources. It excludes specialized learning that can only be obtained with extensive training and information confined to a single medium.</p>
+                            <p className='text-sm text-[#878A99] font-normal text-white text-left'>General knowledge is information that has been accumulated over time through various mediums and sources. It excludes specialized learning that can only be obtained with extensive training and information confined to a single medium.</p>
                         </div>
                     </div>
                     <div className='p-1 flex-1'>
