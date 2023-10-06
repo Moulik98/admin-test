8eb4120eb7e22b80620df7d752fcc6c52a88866a
neo062 <navinkumar241999@gmail.com>
2023-10-06 18:48:05 +0530

    startup cms

--- src/admin-panel/starup-cms/Banners.jsx
+++ src/admin-panel/starup-cms/Banners.jsx
@@ -1,108 +1,81 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import toast from 'react-hot-toast'
 import { Link } from 'react-router-dom'
 import { User } from '../user/User'
 import UploadIcon from './UploadIcon'
 const Banners = () => {
-    const [isClicked, setIsClicked] = useState(false)
-    const [isLoading, setIsLoading] = useState(false)
-    const [main_img, setMain_img] = useState("");
-    const [img_2, setImg_2] = useState("");
-    const [img_3, setImg_3] = useState("");
-    const [img_4, setImg_4] = useState("");
-    const [img_5, setImg_5] = useState("");
-    const [img_6, setImg_6] = useState("");
-    const [img_7, setImg_7] = useState("");
-    const [img_8, setImg_8] = useState("");
+    const [images, setImages] = useState([])
+    const [isUploaded, setIsuploaded] = useState(false)
+    useEffect(() => {
+        const apiUrl = `${process.env.REACT_APP_URL}/v1/cms/getBanner`
+        fetch(apiUrl).then(res => res.json()).then(data => {
+            setImages(data.data)
+        })
+    }, [isUploaded])
 
-    const [main_img_preview, setMain_img_preview] = useState(null);
-    const [img_2_preview, setImg_2_preview] = useState(null);
-    const [img_3_preview, setImg_3_preview] = useState(null);
-    const [img_4_preview, setImg_4_preview] = useState(null);
-    const [img_5_preview, setImg_5_preview] = useState(null);
-    const [img_6_preview, setImg_6_preview] = useState(null);
-    const [img_7_preview, setImg_7_preview] = useState(null);
-    const [img_8_preview, setImg_8_preview] = useState(null);
+    async function makePostRequest(url, data) {
+        console.log('url,data', url, data);
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    // You can add other headers here if needed
+                },
+                body: JSON.stringify(data), // Convert the data to JSON format
+            });
+            if (response.ok) {
+                setIsuploaded(preValue => !preValue)
+            }
+            const responseBody = await response.json(); // Parse the JSON response
 
-    const handleInputChange = (event) => {
-        const { name, files } = event.target;
+            return responseBody;
+        } catch (error) {
+            console.error('Error:', error);
+            throw error;
+        }
+    }
+
+    const handleUpdate = async (e, id) => {
+        const apiUrl = `${process.env.REACT_APP_URL}/v1/cms/startup-banner-update/${id}`
+        const { files } = e.target;
         const file = files[0];
         const reader = new FileReader();
         reader.readAsDataURL(file);
         reader.onload = () => {
-            switch (name) {
-                case "main_img":
-                    setMain_img(reader.result);
-                    setMain_img_preview(URL.createObjectURL(file));
-                    break;
-                case "img_2":
-                    setImg_2(reader.result);
-                    setImg_2_preview(URL.createObjectURL(file));
-                    break;
-                case "img_3":
-                    setImg_3(reader.result);
-                    setImg_3_preview(URL.createObjectURL(file));
-                    break;
-                case "img_4":
-                    setImg_4(reader.result);
-                    setImg_4_preview(URL.createObjectURL(file));
-                    break;
-                case "img_5":
-                    setImg_5(reader.result);
-                    setImg_5_preview(URL.createObjectURL(file));
-                    break;
-                case "img_6":
-                    setImg_6(reader.result);
-                    setImg_6_preview(URL.createObjectURL(file));
-                    break;
-                case "img_7":
-                    setImg_7(reader.result);
-                    setImg_7_preview(URL.createObjectURL(file));
-                    break;
-                case "img_8":
-                    setImg_8(reader.result);
-                    setImg_8_preview(URL.createObjectURL(file));
-                    break;
-                default:
-                    break;
-            }
+            const body = { image: reader.result }
+            makePostRequest(apiUrl, body)
+                .then(responseData => {
+                    if (!responseData.success) {
+                        toast.error(responseData.message)
+                    }
+                    console.log('Response data:', responseData);
+                })
+                .catch(error => {
+                    console.error('Request failed:', error);
+                });
         };
-    };
-    const handleSubmit = (event) => {
-        event.preventDeafault()
-        // setIsLoading(true)
-        // console.log("hii");
-        // event.preventDefault();
-        // const data = {
-        //     product_id: id,
-        //     main_img,
-        //     img_2,
-        //     img_3,
-        //     img_4,
-        //     img_5,
-        //     img_6,
-        //     img_7,
-        //     img_8,
-        // };
-        // console.log(data);
-        // axios
-        //     .post(`${process.env.REACT_APP_URL}/v1/product-images/add`, data, {
-        //         headers: {
-        //             'Content-Type': 'application/json',
-        //             'Authorization': `Bearer ${accessToken}`  // Replace YOUR_TOKEN_HERE with your actual Bearer token
-        //         }
-        //     })
-        //     .then((response) => {
-        //         if (response.status === 200) {
-        //             alert('Image Uploaded Successfully')
-        //             setIsLoading(false)
-        //         }
-        //         console.log(response);
-        //     })
-        //     .catch((error) => {
-        //         console.log(error);
-        //         setIsLoading(false)
-        //         alert(error)
-        //     });
+    }
+    const handleStatus = async (id) => {
+        const apiUrl = `${process.env.REACT_APP_URL}/v1/cms/toggleBannerStatus/${id}`;
+
+        try {
+            const res = await fetch(apiUrl, {
+                method: "POST",
+            });
+
+            if (res.status === 200) {
+                // Handle success
+                toast.success("Status toggled successfully.");
+                setIsuploaded(preValue => !preValue)
+            } else {
+                // Handle other status codes
+                toast.error("Status toggle failed.");
+            }
+        } catch (error) {
+            // Handle network errors or exceptions
+            console.error("An error occurred:", error);
+        }
     };
 
     return (
@@ -143,190 +116,46 @@ const Banners = () => {
                 </div>
             </section>
             <section className='w-full flex flex-wrap '>
-                <div className="w-full mt-4 gap-4 ">
-                    <form onSubmit={(e) => handleSubmit(e)} className="w-full">
-                        <div className="w-full grid grid-cols-4 grid-flow-row gap-4 px-8 py-6">
-                            <div className="relative">
-                                <input
-                                    type="file"
-                                    name="main_img"
-                                    id="main_img"
-                                    onChange={handleInputChange}
-                                    className="border bg-[#e2eced] border-gray-400 h-8 w-full rounded-md px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                />
-                                <div className="w-full h-40 bg-[#e2eced] flex items-center justify-center">
-                                    <UploadIcon />
+                <div className="w-full grid grid-cols-4 grid-flow-row gap-4 px-8 py-6">
+                    {
+                        images?.map(item => {
+                            const fillColor = item.status ? 'red' : 'none'
+                            const textColor = item.status ? 'text-red-500' : 'text-black'
+                            return (
 
-                                    {main_img_preview && (
-                                        <img
-                                            src={main_img_preview}
-                                            alt="Main Image Preview"
-                                            className="w-full h-full object-cover rounded-md"
-                                        />
-                                    )}
+                                <div className="relative">
+                                    <input
+                                        type="file"
+                                        name="main_img"
+                                        id="main_img"
+                                        onChange={(e) => handleUpdate(e, item._id)}
+                                        className="border bg-[#e2eced] border-gray-400 h-8 w-full rounded-md px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                    />
+                                    <div
+                                        onClick={() => handleStatus(item._id)}
+                                        className='cursor-pointer absolute -top-5 -right-5'>
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill={fillColor} viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className={`${textColor} w-6 h-6`}>
+                                            <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                                        </svg>
+
+                                    </div>
+                                    <div className="w-full h-40 bg-[#e2eced] flex items-center justify-center">
+
+                                        {item.image ?
+                                            <img
+                                                src={item.image}
+                                                alt="Main"
+                                                className="w-full h-full object-cover rounded-md"
+                                            />
+                                            : <UploadIcon />
+                                        }
+
+
+                                    </div>
                                 </div>
-                            </div>
-
-                            <div className="relative">
-                                <input
-                                    type="file"
-                                    name="img_2"
-                                    id="img_2"
-                                    onChange={handleInputChange}
-                                    className="border bg-[#e2eced] border-gray-400 h-8 w-full rounded-md px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                />
-                                <div className="w-full h-40 bg-[#e2eced] flex items-center justify-center">
-                                    <UploadIcon />
-
-                                    {img_2_preview && (
-                                        <img
-                                            src={img_2_preview}
-                                            alt="Main Image Preview"
-                                            className="w-full h-full object-cover rounded-md"
-                                        />
-                                    )}
-                                </div>
-                            </div>
-
-                            <div className="relative">
-                                <input
-                                    type="file"
-                                    name="img_3"
-                                    id="img_3"
-                                    onChange={handleInputChange}
-                                    className="border bg-[#e2eced] border-gray-400 h-8 w-full rounded-md px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                />
-                                <div className="w-full h-40 bg-[#e2eced] flex items-center justify-center">
-                                    <UploadIcon />
-
-                                    {img_3_preview && (
-                                        <img
-                                            src={img_3_preview}
-                                            alt="Main Image Preview"
-                                            className="w-full h-full object-cover rounded-md"
-                                        />
-                                    )}
-                                </div>
-                            </div>
-
-                            <div className="relative">
-                                <input
-                                    type="file"
-                                    name="img_4"
-                                    id="img_4"
-                                    onChange={handleInputChange}
-                                    className="border bg-[#e2eced] border-gray-400 h-8 w-full rounded-md px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                />
-                                <div className="w-full h-40 bg-[#e2eced] flex items-center justify-center">
-                                    <UploadIcon />
-
-                                    {img_4_preview && (
-                                        <img
-                                            src={img_4_preview}
-                                            alt="Main Image Preview"
-                                            className="w-full h-full object-cover rounded-md"
-                                        />
-                                    )}
-                                </div>
-                            </div>
-
-                            <div className="relative">
-                                <input
-                                    type="file"
-                                    name="img_5"
-                                    id="img_5"
-                                    onChange={handleInputChange}
-                                    className="border bg-[#e2eced] border-gray-400 h-8 w-full rounded-md px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                />
-                                <div className="w-full h-40 bg-[#e2eced] flex items-center justify-center">
-                                    <UploadIcon />
-
-                                    {img_5_preview && (
-                                        <img
-                                            src={img_5_preview}
-                                            alt="Main Image Preview"
-                                            className="w-full h-full object-cover rounded-md"
-                                        />
-                                    )}
-                                </div>
-                            </div>
-
-                            <div className="relative">
-                                <input
-                                    type="file"
-                                    name="img_6"
-                                    id="img_6"
-                                    onChange={handleInputChange}
-                                    className="border bg-[#e2eced] border-gray-400 h-8 w-full rounded-md px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                />
-                                <div className="w-full h-40 bg-[#e2eced] flex items-center justify-center">
-                                    <UploadIcon />
-
-                                    {img_6_preview && (
-                                        <img
-                                            src={img_6_preview}
-                                            alt="Main Image Preview"
-                                            className="w-full h-full object-cover rounded-md"
-                                        />
-                                    )}
-                                </div>
-                            </div>
-
-                            <div className="relative">
-                                <input
-                                    type="file"
-                                    name="img_7"
-                                    id="img_7"
-                                    onChange={handleInputChange}
-                                    className="border bg-[#e2eced] border-gray-400 h-8 w-full rounded-md px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                />
-                                <div className="w-full h-40 bg-[#e2eced] flex items-center justify-center">
-                                    <UploadIcon />
-
-                                    {img_7_preview && (
-                                        <img
-                                            src={img_7_preview}
-                                            alt="Main Image Preview"
-                                            className="w-full h-full object-cover rounded-md"
-                                        />
-                                    )}
-                                </div>
-                            </div>
-
-                            <div className="relative">
-                                <input
-                                    type="file"
-                                    name="img_8"
-                                    id="img_8"
-                                    onChange={handleInputChange}
-                                    className="border bg-[#e2eced] border-gray-400 h-8 w-full rounded-md px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-                                />
-
-                                <div className="w-full h-40 bg-[#e2eced] flex items-center justify-center">
-                                    <UploadIcon />
-
-                                    {img_8_preview && (
-                                        <img
-                                            src={img_8_preview}
-                                            alt="Main Image Preview"
-                                            className="w-full h-full object-cover rounded-md"
-                                        />
-                                    )}
-                                </div>
-                            </div>
-
-                        </div>
-
-                        <div className="flex justify-end max-w-5xl  mx-10">                                                 <button
-                            className="text-white text-xs  font-normal border border-solid border-[#E3ECED] shadow-[0px,1px,2px,#B5B5B5] rounded-sm py-2 px-6 bg-[#008296] inline-block "
-                            type="submit"
-                        >
-
-                            Save
-                        </button>
-
-                        </div>
-                    </form>
+                            )
+                        })
+                    }
                 </div>
             </section>
         </main>
