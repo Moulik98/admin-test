8bbde633376c796c1321dc35e984a90c54048d42
Shauryaditya <shauryadityab@gmail.com>
2023-10-10 11:49:38 +0530

    coupon search

--- src/admin-panel/coupons/Coupons.jsx
+++ src/admin-panel/coupons/Coupons.jsx
@@ -5,6 +5,10 @@ import { Link } from 'react-router-dom'
 import { User } from '../user/User'
 const Coupons = () => {
     const [isClicked, setIsClicked] = useState(false)
+      // Add a new state for search results
+  const [searchQuery, setSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState([]);
+  const [showDropdown, setShowDropDown] = useState(false);
 
     const [couponId, setCouponId] = useState('')
     const handleNewClick = (id) => {
@@ -15,6 +19,8 @@ const Coupons = () => {
     const handleClose = () => {
         setIsClicked(false)
     }
+
+
     return (
         <main>
             <div className='pr-6'>
@@ -22,33 +28,7 @@ const Coupons = () => {
                     <div className='max-w-6xl mx-auto flex justify-between py-5'>
                         <Link to='/dashboard' className='text-3xl text-gray-900 font-semibold'>{`${isClicked ? 'Coupons' : 'Coupons List'}`}</Link>
                         <div className='flex gap-x-10'>
-                            <form className="flex items-center">
-                                <div className="flex items-center px-2 py-1 gap-x-1 bg-gray-100 rounded-2xl ">
-                                    <div className=' bg-white rounded-full p-1'>
-                                        <svg
-                                            xmlns="http://www.w3.org/2000/svg"
-                                            fill="none"
-                                            viewBox="0 0 24 24"
-                                            strokeWidth="1.5"
-                                            stroke="currentColor"
-                                            className="w-4 h-4"
-                                        >
-                                            <path
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                                d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
-                                            />
-                                        </svg>
-                                    </div>
-                                    <input
-                                        className="w-52 py-1 px-1 bg-gray-100 outline-0"
-                                        //   value={searchTerm}
-                                        //   onChange={handleSearch}
-                                        type="text"
-                                    />
-
-                                </div>
-                            </form>
+                     
                             <User/>
                         </div>
                     </div>
