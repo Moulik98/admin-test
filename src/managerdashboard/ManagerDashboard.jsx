b4c36d42ae6be0566b18958a8aa73744941d23d1
Shauryaditya <shauryadityab@gmail.com>
2023-11-03 18:51:18 +0530

    category manger profile update

--- src/managerdashboard/ManagerDashboard.jsx
+++ src/managerdashboard/ManagerDashboard.jsx
@@ -2,13 +2,54 @@ import React from 'react'
 import ManageStats from './ManagerStats'
 import SellerTable from './SellerTable'
 import OnboardedSellers from './OnboardedSellers'
+import SideBar from './Sidebar'
+import Layout from '../Routing/Layout'
 
 
 
 const ManagerDashboard = () => {
 
-  return (
-    <main className='max-w-full mx-8'>
+  const categoryMenu = [
+    {
+        title: "Onboard Seller",
+        src: (
+            <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
+            </svg>
+
+        ),
+        link: "/onboard-seller",
+    },
+    {
+        title: "Profile",
+        src: (
+            <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+            </svg>
+
+        ),
+        link: "/CM-Info",
+    },
+    {
+        title: "Dashboard",
+        src: (
+            <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 01-1.125-1.125M3.375 19.5h7.5c.621 0 1.125-.504 1.125-1.125m-9.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-7.5A1.125 1.125 0 0112 18.375m9.75-12.75c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125m19.5 0v1.5c0 .621-.504 1.125-1.125 1.125M2.25 5.625v1.5c0 .621.504 1.125 1.125 1.125m0 0h17.25m-17.25 0h7.5c.621 0 1.125.504 1.125 1.125M3.375 8.25c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m17.25-3.75h-7.5c-.621 0-1.125.504-1.125 1.125m8.625-1.125c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M12 10.875v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125M13.125 12h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125M20.625 12c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5M12 14.625v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 14.625c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m0 1.5v-1.5m0 0c0-.621.504-1.125 1.125-1.125m0 0h7.5" />
+            </svg>
+
+
+        ),
+        link: "/category-head-dashboard",
+    }
+]
+
+return (
+<main className='max-w-full flex'>
+  <div className="sidebar bg-[#00388c] h-screen w-fit sticky top-0">
+    <SideBar menu={categoryMenu} />
+    </div>
+    <div className='max-w-full grow mx-8'>
+      
     <section className='flex flex-row justify-between py-5 items-center'>
     <div>
         <h1 className=' text-2xl md:text-3xl text-[#383E50] font-semibold leading-10'>Dashboard</h1>
@@ -32,6 +73,7 @@ const ManagerDashboard = () => {
     </div>
     </section>
 
+</div>
 </main>
   )
 }
