345e0403271c01c1f1e12b5ee57de892586938d4
Moulik98 <soumamoulik@gmail.com>
2023-11-13 17:43:41 +0530

    update

--- src/managerdashboard/qaa/QaProfile.jsx
+++ src/managerdashboard/qaa/QaProfile.jsx
@@ -113,7 +113,7 @@ const QaProfile = () => {
       </div>
       <form className="grow p-8" onSubmit={(e) => handleSubmit(e)}>
         <h1 className="text-xl text-left font-bold">
-          Category Manager Profile
+          QA Approver Profile
         </h1>
         <div className="grid grid-cols-2 gap-5 mt-8">
           {inputFields.map((field, index) => (
