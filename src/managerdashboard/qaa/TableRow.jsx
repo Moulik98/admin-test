5af84cfcacb4c371b687f9c7c90b030be70fbb13
Moulik98 <soumamoulik@gmail.com>
2023-11-23 18:53:16 +0530

    update

--- src/managerdashboard/qaa/TableRow.jsx
+++ src/managerdashboard/qaa/TableRow.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import AttachmentModal from "./AttachmentModal";
 
 const TableRow = ({ data, index }) => {
-  const { _id, fullname, email, store_name, isVerify } = data.seller;
+  const { _id, fullname, store_name,sellerType, isVerify ,seller_code} = data.seller;
+  const {onboarded_date } = data
   const { name } = data.cm;
   const [viewAttachment, setViewAttachment] = useState(false);
   const [showDeletePopup, setShowDeletePopup] = useState(false);
@@ -27,10 +28,10 @@ const TableRow = ({ data, index }) => {
     <tr key={_id}>
       <td className="px-4 py-2">{index+1}</td>
       <td className="px-4 py-2">{fullname}</td>
-      <td className="px-4 py-2">23/05/2022</td>
-      <td className="px-4 py-2">GX00259785</td>
+      <td className="px-4 py-2">{new Date(onboarded_date).toDateString()}</td>
+      <td className="px-4 py-2">{seller_code}</td>
       <td className="px-4 py-2">{store_name}</td>
-      <td className="px-4 py-2">Startup</td>
+      <td className="px-4 py-2">{sellerType}</td>
       <td className="px-4 py-2">{name}</td>
       <td className="px-4 py-2">
         <div
