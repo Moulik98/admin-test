a13244639f302848ae53a397b595bf47542e54ca
Shauryaditya <shauryadityab@gmail.com>
2023-11-24 19:30:03 +0530

    Order refund

--- src/admin-panel/orders/OrdersTableRow.jsx
+++ src/admin-panel/orders/OrdersTableRow.jsx
@@ -1,12 +1,15 @@
 import React, { useState } from 'react'
 import ViewModal from './ViewModal'
+import RefundModal from './RefundModal'
 const OrdersTableRow = ({ data, onDelete }) => {
-    const { _id, user_order_id, order_status, payment_method, payment_status, voucher, seller_details, amount, order_date } = data
-
+    const { _id, user_order_id, order_status, payment_method,payment_id, payment_status, voucher, seller_details, amount, order_date } = data
+    console.log("Order data",data)
     // state for viewModa 
     const [viewModal, setViewModal] = useState(false)
+    const [refundModal, setRefundModal] = useState(false)
     const handleClose = () => {
         setViewModal(false)
+        setRefundModal(false)
     }
     const token = localStorage.getItem('access_token')
 
@@ -42,15 +45,29 @@ const OrdersTableRow = ({ data, onDelete }) => {
             <td class="px-2 py-2">{seller_details && seller_details[0]?.fullname}</td>
             <td class="px-2 py-2">₹{amount}</td>
             <td class="px-2 py-2">{order_date}</td>
+            <td className="px-2 py-2">
+                {(order_status === 'returned' || order_status === 'cancelled') && payment_status === 'paid' ? (
+                    <button className='bg-red-500 px-2 py-1 text-white' onClick={() => setRefundModal(true)}>Issue Refund</button>
+                ) : null}
+                {
+                    refundModal && 
+                    <RefundModal  
+                    order_id={user_order_id} 
+                    payment_id={payment_id}
+                    amount={amount}
+                    visible={refundModal}
+                    onClose={handleClose}/>
+                }
+            </td>
             <td class="px-2 py-2">
                 <div class="flex gap-x-2">
                     <div
                         onClick={() => setViewModal(true)}
                         class="rounded bg-[#E8F0FE] p-2 text-xs font-bold text-[#4285F4] cursor-pointer"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
-  <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-</svg>
-</div>
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                        </svg>
+                    </div>
                     {/* <div
                         onClick={() => handleDelete()}
                         class="rounded bg-[#F7D7DB] p-2 text-xs font-bold text-[#A12321] cursor-pointer">Delete
@@ -58,6 +75,7 @@ const OrdersTableRow = ({ data, onDelete }) => {
                     {
                         viewModal && <ViewModal
                             id={_id}
+                       
                             visible={viewModal}
                             onClose={handleClose}
                         />
