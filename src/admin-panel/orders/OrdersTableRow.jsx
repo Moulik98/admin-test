94712e16f1ec1c8718abcae9812d944ca1ef7ff3
Shauryaditya <shauryadityab@gmail.com>
2023-11-28 12:24:13 +0530

    removing special characters

--- src/admin-panel/orders/OrdersTableRow.jsx
+++ src/admin-panel/orders/OrdersTableRow.jsx
@@ -41,7 +41,7 @@ const OrdersTableRow = ({ data, onDelete }) => {
     return (
         <tr class="border-b border-solid border-gray-200 bg-white hover:bg-gray-50 text-[#222222]">
             <td scope="row" class="whitespace-nowrap px-2 py-2 font-medium text-gray-900">{user_order_id}</td>
-            <td class="px-2 py-2 capitalize">{order_status}</td>
+            <td class="px-2 py-2 capitalize">{order_status.replace(/_/g, " ")}</td>
             <td class="px-2 py-2">{payment_method}</td>
             <td class="px-2 py-2 capitalize"><div className={`p-1 text-center rounded-md capitalize ${payment_status === 'paid' ? 'bg-green-100' : 'bg-red-100'}`}>{payment_status}</div></td>
             <td scope="row" class="whitespace-nowrap px-2 py-2 font-medium text-gray-900">{voucher && voucher[0]?.coupon_code}</td>
