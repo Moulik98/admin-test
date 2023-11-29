cfdc71cd092d91f92bc08303670d5f9e164f4f2e
neo062 <navinkumar241999@gmail.com>
2023-11-29 14:49:13 +0530

    marketing head associate brand table and associate prouct table

--- src/managerdashboard/mh/AssociateMmTable.jsx
+++ src/managerdashboard/mh/AssociateMmTable.jsx
@@ -1,49 +1,15 @@
-import React, { useEffect, useState, useCallback } from "react";
-import { format } from "date-fns";
-import MergeButton from "./MergeButton";
+import React, { useCallback } from "react";
+
 import EyeButton from "./EyeButton";
 import { useNavigate } from "react-router-dom";
-import { getToken } from "../../hook/getToken";
-import Pagination from "../../Pagination";
-import Description from "../../Description";
 
-const AssociateMmTable = ({ cmName, id }) => {
+
+const AssociateMmTable = ({ cmName, id, list }) => {
   const navigate = useNavigate();
-  const [apiData, setApiData] = useState([]);
-
-  const fetchData = async () => {
-    try {
-      const accessToken = getToken();
-
-      const response = await fetch(
-        `${process.env.REACT_APP_URL}/v1/mh/get-mm-list/${id}`,
-        {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${accessToken}`,
-          },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-
-      const data = await response.json();
-      setApiData(data);
-    } catch (error) {
-      console.error("Error fetching data:", error);
-    }
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
 
   const handleClick = useCallback((id) => {
-    const url = `/category-head-dashboard/associate-seller/${id}`;
-    console.log("url", url);
-    // navigate(url);
+    const url = `/marketing-head-dashboard/associate-brands/products/${id}`;
+    navigate(url);
   }, []);
 
   return (
@@ -55,102 +21,43 @@ const AssociateMmTable = ({ cmName, id }) => {
               Sl. No
             </th>
             <th scope="col" className="px-4 py-2">
-              Brand
+              Brand Title
             </th>
             <th scope="col" className="px-4 py-2">
-              Product Name
+              MM Name (ID)
             </th>
             <th scope="col" className="px-4 py-2">
-              Product ID
+              No: Product
             </th>
             <th scope="col" className="px-4 py-2">
-              Category Tags
-            </th>
-            <th scope="col" className="px-4 py-2">
-              Utility Tags
-            </th>
-
-            <th scope="col" className="px-4 py-2">
-              Actions
+              Action (view)
             </th>
           </tr>
         </thead>
         <tbody>
-          {Array.isArray(apiData) &&
-            apiData.map((brand, brandIndex) => {
-              const {
-                _id: brandId,
-                brand_name,
-                brand_logo_url,
-                products,
-              } = brand;
-
-              // Combine product names, external IDs, category tags, and utility tags
-              const combinedProductInfo = products.reduce(
-                (acc, product, productIndex) => {
-                  const {
-                    item_name,
-                    product_external_id,
-                    category_tags,
-                    utility_tags,
-                  } = product;
-
-                  if (productIndex > 0) {
-                    acc.productNames += ",";
-                    acc.externalIds.push(", ");
-                    acc.categoryTags.push(", ");
-                    acc.utilityTags.push(", ");
-                  }
-
-                  acc.productNames += item_name;
-                  acc.externalIds.push(product_external_id);
-                  acc.categoryTags.push(...(category_tags || []));
-                  acc.utilityTags.push(...(utility_tags || []));
-
-                  return acc;
-                },
-                {
-                  productNames: "",
-                  externalIds: [],
-                  categoryTags: [],
-                  utilityTags: [],
-                }
-              );
+          {Array.isArray(list) &&
+            list?.map((brand, brandIndex) => {
+              const { _id, brand_name, cm_data, product_count } = brand
 
               return (
-                <tr key={brandId}>
+                <tr key={_id}>
                   <td className="px-4 py-2">{brandIndex + 1}</td>
                   <td className="px-4 py-2">
-                    <div className="flex">
-                      {brand_name}{" "}
-                      <img
-                        src={brand_logo_url}
-                        alt={brand_name}
-                        style={{ width: "20px", height: "20px" }}
-                      />
-                    </div>
+                    {brand_name}
                   </td>
                   <td className="px-4 py-2">
-                    <Description description={combinedProductInfo.productNames}/>
+                    {cm_data?.name} ({cm_data?.emp_id})
                   </td>
                   <td className="px-4 py-2">
-                  <Description description={combinedProductInfo.externalIds.join(",")}/>
+                    {product_count}
                   </td>
-                  <td className="px-4 py-2">
-                  <Description description={combinedProductInfo.categoryTags.join(",")}/>
-                  </td>
-                  <td className="px-4 py-2">
-                  <Description description={combinedProductInfo.utilityTags.join(",")}/>
-                  </td>
-
                   <td className="px-4 py-2">
                     <div className="flex gap-x-2 px-4">
                       <EyeButton
-                        id={brandId}
-                        onClick={() => handleClick(brandId)}
+                        id={_id}
+                        onClick={() => handleClick(_id)}
                       />
-                      {/* Assuming MergeButton requires brandId */}
-                      <MergeButton brandId={brandId} />
+
                     </div>
                   </td>
                 </tr>
