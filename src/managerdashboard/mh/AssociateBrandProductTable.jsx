4b1d8a3f88fc991fc525619fff1260ab3b6f6e1f
Moulik98 <soumamoulik@gmail.com>
2023-12-15 15:30:23 +0530

    update table

--- src/managerdashboard/mh/AssociateBrandProductTable.jsx
+++ src/managerdashboard/mh/AssociateBrandProductTable.jsx
@@ -3,89 +3,89 @@ import Description from "../../Description";
 import EyeButton from "./EyeButton";
 import { useNavigate } from "react-router-dom";
 
-
 const AssociateBrandProductTable = ({ brandName, id, list }) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleClick = useCallback((id) => {
-        const url = `/marketing-head-dashboard/associate-brands/products/${id}`;
-        console.log("url", url);
-        // navigate(url);
-    }, []);
+  const handleClick = useCallback((id) => {
+    const url = `/marketing-head-dashboard/associate-brands/products/${id}`;
+    console.log("url", url);
+    // navigate(url);
+  }, []);
 
-    if (!list) return <h1 className="text-center my-5">This Brand does not have product</h1>
-    return (
-        <div className="relative w-full overflow-x-scroll">
-            <table className="w-full overflow-x-scroll text-left text-xs">
-                <thead className="bg-gray-100 text-xs font-medium uppercase text-[#666666]">
-                    <tr>
-                        <th scope="col" className="px-4 py-2">
-                            Sl. No
-                        </th>
-                        <th scope="col" className="px-4 py-2">
-                            Product Title (ID)
-                        </th>
-                        <th scope="col" className="px-4 py-2">
-                            Category Tags
-                        </th>
-                        <th scope="col" className="px-4 py-2">
-                            Utility Tags
-                        </th>
-                        <th scope="col" className="px-4 py-2">
-                            Brand Title
-                        </th>
-                        <th scope="col" className="px-4 py-2">
-                            Action (view)
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {Array.isArray(list) &&
-                        list?.map((brand, brandIndex) => {
-                            const { _id, item_name, product_external_id, utility_tags, category_tags } = brand
-                            const utilityTag = utility_tags.join(', ')
-                            const categoryTag = category_tags.join(', ')
-                            return (
-                                <tr key={_id}>
-                                    <td className="px-4 py-2">{brandIndex + 1}</td>
-                                    <td className="px-4 py-2">
-                                        {item_name} ({product_external_id})
-                                    </td>
-                                    <td className="px-4 py-2">
-                                        <div className="flex flex-wrap gap-1">
-                                            <Description
-                                                description={categoryTag}
-
-                                            />
-                                        </div>
-                                    </td>
-                                    <td className="px-4 py-2">
-                                        <div className="flex flex-wrap gap-1">
-                                            <Description
-                                                description={utilityTag}
-
-                                            />
-                                        </div>
-                                    </td>
-                                    <td className="px-4 py-2">
-                                        {brandName}
-                                    </td>
-                                    <td className="px-4 py-2">
-                                        <div className="flex gap-x-2 px-4">
-                                            <EyeButton
-                                                id={_id}
-                                                onClick={() => handleClick(_id)}
-                                            />
-
-                                        </div>
-                                    </td>
-                                </tr>
-                            );
-                        })}
-                </tbody>
-            </table>
-        </div>
-    );
+  return (
+    <div className="relative w-full overflow-x-scroll">
+      <table className="w-full overflow-x-scroll text-left text-xs">
+        <thead className="bg-gray-100 text-xs font-medium uppercase text-[#666666]">
+          <tr>
+            <th scope="col" className="px-4 py-2">
+              Sl. No
+            </th>
+            <th scope="col" className="px-4 py-2">
+              Product Title (ID)
+            </th>
+            <th scope="col" className="px-4 py-2">
+              Category Tags
+            </th>
+            <th scope="col" className="px-4 py-2">
+              Utility Tags
+            </th>
+            <th scope="col" className="px-4 py-2">
+              Brand Title
+            </th>
+            <th scope="col" className="px-4 py-2">
+              Action (view)
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.isArray(list) && list.length > 0 ? (
+            list.map((brand, brandIndex) => {
+              const {
+                _id,
+                item_name,
+                product_external_id,
+                utility_tags,
+                category_tags,
+              } = brand;
+              const utilityTag = utility_tags.join(", ");
+              const categoryTag = category_tags.join(", ");
+              return (
+                <tr key={_id}>
+                  <td className="px-4 py-2">{brandIndex + 1}</td>
+                  <td className="px-4 py-2">
+                    {item_name} ({product_external_id})
+                  </td>
+                  <td className="px-4 py-2">
+                    <div className="flex flex-wrap gap-1">
+                      <Description description={categoryTag} />
+                    </div>
+                  </td>
+                  <td className="px-4 py-2">
+                    <div className="flex flex-wrap gap-1">
+                      <Description description={utilityTag} />
+                    </div>
+                  </td>
+                  <td className="px-4 py-2">{brandName}</td>
+                  <td className="px-4 py-2">
+                    <div className="flex gap-x-2 px-4">
+                      <EyeButton
+                        id={_id}
+                        onClick={() => handleClick(_id)}
+                      />
+                    </div>
+                  </td>
+                </tr>
+              );
+            })
+          ) : (
+            <tr>
+              <td colSpan="24" className="text-center py-2" >No products available</td>
+            </tr>
+          )}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 
 export default AssociateBrandProductTable;
--- src/managerdashboard/mh/AssociateMmTable.jsx
+++ src/managerdashboard/mh/AssociateMmTable.jsx
@@ -1,8 +1,6 @@
 import React, { useCallback } from "react";
-
 import EyeButton from "./EyeButton";
 import { useNavigate } from "react-router-dom";
-
 
 const AssociateMmTable = ({ cmName, id, list }) => {
   const navigate = useNavigate();
@@ -14,57 +12,52 @@ const AssociateMmTable = ({ cmName, id, list }) => {
 
   return (
     <div className="relative w-full overflow-x-scroll">
-      <table className="w-full overflow-x-scroll text-left text-xs">
-        <thead className="bg-gray-100 text-xs font-medium uppercase text-[#666666]">
-          <tr>
-            <th scope="col" className="px-4 py-2">
-              Sl. No
-            </th>
-            <th scope="col" className="px-4 py-2">
-              Brand Title
-            </th>
-            <th scope="col" className="px-4 py-2">
-              MM Name (ID)
-            </th>
-            <th scope="col" className="px-4 py-2">
-              No: Product
-            </th>
-            <th scope="col" className="px-4 py-2">
-              Action (view)
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          {Array.isArray(list) &&
-            list?.map((brand, brandIndex) => {
-              const { _id, brand_name, cm_data, product_count } = brand
+      {Array.isArray(list) && list.length > 0 ? (
+        <table className="w-full overflow-x-scroll text-left text-xs">
+          <thead className="bg-gray-100 text-xs font-medium uppercase text-[#666666]">
+            <tr>
+              <th scope="col" className="px-4 py-2">
+                Sl. No
+              </th>
+              <th scope="col" className="px-4 py-2">
+                Brand Title
+              </th>
+              <th scope="col" className="px-4 py-2">
+                MM Name (ID)
+              </th>
+              <th scope="col" className="px-4 py-2">
+                No: Product
+              </th>
+              <th scope="col" className="px-4 py-2">
+                Action (view)
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {list.map((brand, brandIndex) => {
+              const { _id, brand_name, cm_data, product_count } = brand;
 
               return (
                 <tr key={_id}>
                   <td className="px-4 py-2">{brandIndex + 1}</td>
-                  <td className="px-4 py-2">
-                    {brand_name}
-                  </td>
+                  <td className="px-4 py-2">{brand_name}</td>
                   <td className="px-4 py-2">
                     {cm_data?.name} ({cm_data?.emp_id})
                   </td>
-                  <td className="px-4 py-2">
-                    {product_count}
-                  </td>
+                  <td className="px-4 py-2">{product_count}</td>
                   <td className="px-4 py-2">
                     <div className="flex gap-x-2 px-4">
-                      <EyeButton
-                        id={_id}
-                        onClick={() => handleClick(_id)}
-                      />
-
+                      <EyeButton id={_id} onClick={() => handleClick(_id)} />
                     </div>
                   </td>
                 </tr>
               );
             })}
-        </tbody>
-      </table>
+          </tbody>
+        </table>
+      ) : (
+        <p>No brands available</p>
+      )}
     </div>
   );
 };
