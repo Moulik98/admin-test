eac8937e7566a00c194f85f2cd8cbcd00cb4f9a0
neo062 <navinkumar241999@gmail.com>
2023-12-28 11:34:32 +0530

    Modal : new three component

--- src/admin-panel/cms/content-manager/preview/ComponentRegistry.jsx
+++ src/admin-panel/cms/content-manager/preview/ComponentRegistry.jsx
@@ -5,12 +5,32 @@ import SingleImageSidebar from './SingleImageSidebar';
 import LeftImageWithText from './LeftImageWithText';
 import RightImageWithText from './RightImageWithText';
 import ImageSlider from './ImageSlider';
+import BGImageDarkTextOverlay from './BGImageDarkTextOverlay'
+import BGImageLightTextOverlay from './BGImageLightTextOverlay'
+import BigBanner from './BigBanner'
+import BigHeadingAndDescription from './BigHeadingAndDescription'
+import CompanyLogo from './CompanyLogo'
+import GridFiveImage from './GridFiveImage'
+import GridTwoImage from './GridTwoImage'
+import LeftRightImageWithDescription from './LeftRightImageWithDescription'
+import ProductDescription from './ProductDescription'
+
 const componentRegistry = {
     ImageHeaderWithText,
     SingleImageSidebar,
     LeftImageWithText,
     RightImageWithText,
-    ImageSlider
+
+    ImageSlider,
+    BGImageDarkTextOverlay,
+    BGImageLightTextOverlay,
+    // BigBanner,
+    // BigHeadingAndDescription,
+    // CompanyLogo,
+    // GridFiveImage,
+    // GridTwoImage,
+    // LeftRightImageWithDescription,
+    // ProductDescription
     // Add other components as needed
 };
 
