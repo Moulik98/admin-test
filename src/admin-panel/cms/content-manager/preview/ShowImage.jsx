83fed3f7d6401b18723f41a3e7686b2ebc894c1a
neo062 <navinkumar241999@gmail.com>
2024-01-03 12:10:50 +0530

    hello there

--- src/admin-panel/cms/content-manager/preview/ShowImage.jsx
+++ src/admin-panel/cms/content-manager/preview/ShowImage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 const ShowImage = ({ image, alt, style }) => {
     return (
         <div className={`${style}`}>
-            <img className='w-full h-full object-contain' alt={alt} src={image} />
+            <img className='w-full h-full object-cover' alt={alt} src={image} />
         </div>
     )
 }
